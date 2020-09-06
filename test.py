ba1c99cdf4dfde18fa5f14b1da4f142f9f01e1ae
DanWangZai <35184693+DanWangZai@users.noreply.github.com>
2020-09-06 13:26:51 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,9 +6,12 @@ def my_print(s: str):
     print("test change")
     print("add changes in dev")
     print("add change from origin master")
-    
-    
+   
 def function_from_origin(a):
+    print("modify the function")
     return a+3
+
+def add_new_function_from_origin(b):
+    return b
     
 
