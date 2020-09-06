ee5152ca94bd2cddbf380a8ed710446833860665
wangzaiwang <danwang208@gmail.com>
2020-09-06 13:17:52 -0500

    add local function to test.py

--- test.py
+++ test.py
@@ -3,5 +3,10 @@ import numpy as np
 
 def my_print(s: str):
     print(s)
+    print("this is to test the rebase")
     print("test change")
     print("add changes in dev")
+
+
+def local_added_function(c):
+    return c
