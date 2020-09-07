052ad3e955f62146a00a2113c9e1b5e7e618b807
wangzaiwang <danwang208@gmail.com>
2020-09-06 19:52:33 -0500

    add trail_1.py

--- trail_1.py
+++ trail_1.py
@@ -1,4 +1,5 @@
-there is no content
+def add_new_content:
+    return 3
 
 def add_from_origin:
     print('edit from local')
@@ -6,3 +7,7 @@ def add_from_origin:
 
 def create_from_local:
     return 2
+
+def add_more:
+    return 4
+
